553de414da72add638b7e66ef51a768a48d9d25b
NegrebLC <BergCu2001@gmail.com>
2024-03-31 21:44:35 -0600

    Models

---
+++ Models/SupportChat/Chat.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace _2211_Final_Project_TGM_Blog.Models.SupportChat
+{
+    public class Chat
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string User1Id { get; set; }
+
+        [Required]
+        public string User2Id { get; set; }
+
+        [Required]
+        public DateTime StartTime { get; set; }
+
+        public DateTime? EndTime { get; set; }
+
+        // Navigation properties for EF Core
+        public virtual ICollection<Message> Messages { get; set; }
+    }
+}
