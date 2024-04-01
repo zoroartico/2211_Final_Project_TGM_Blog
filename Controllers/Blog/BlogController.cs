d8a0e9f7c3d596b4a6d6698bd0516c5f31af280b
Syyren <129496054+Syyren@users.noreply.github.com>
2024-04-01 16:11:00 -0600

    Implemented structure for feature, added migration to db

---
+++ Controllers/Blog/BlogController.cs
@@ -0,0 +1,69 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+
+using _2211_Final_Project_TGM_Blog.Data;
+using _2211_Final_Project_TGM_Blog.Models.Blog;
+
+namespace _2211_Final_Project_TGM_Blog.Controllers.Blog
+{
+    public class BlogController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+        private readonly UserManager<IdentityUser> _userManager;
+
+        public BlogController(ApplicationDbContext context, UserManager<IdentityUser> userManager)
+        {
+            _context = context;
+            _userManager = userManager;
+        }
+
+        public IActionResult Index()
+        {
+            var blogPosts = _context.BlogPosts.ToList();
+            return View(blogPosts);
+        }
+
+        [Authorize(Roles = "Dev")] // Only users in "Dev" role can access this action
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [Authorize(Roles = "Dev")] // Only users in "Dev" role can access this action
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(BlogPost blogPost)
+        {
+            if (ModelState.IsValid)
+            {
+                var user = await _userManager.GetUserAsync(User);
+                blogPost.UserId = user.Id;
+                _context.BlogPosts.Add(blogPost);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(blogPost);
+        }
+
+        [Authorize] // Any authenticated user can like a post
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Like(int postId)
+        {
+            var user = await _userManager.GetUserAsync(User);
+            var like = new Like
+            {
+                UserId = user.Id,
+                BlogPostId = postId
+            };
+            _context.Likes.Add(like);
+            await _context.SaveChangesAsync();
+
+            return RedirectToAction(nameof(Index));
+        }
+    }
+
+}
--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -1,4 +1,5 @@
-﻿using _2211_Final_Project_TGM_Blog.Models.SupportChat;
+﻿using _2211_Final_Project_TGM_Blog.Models.Blog;
+using _2211_Final_Project_TGM_Blog.Models.SupportChat;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
@@ -14,12 +15,23 @@ namespace _2211_Final_Project_TGM_Blog.Data
         public DbSet<Chat> Chats { get; set; }
         public DbSet<Message> Messages { get; set; }
         public DbSet<ChatRequest> ChatRequests { get; set; }
+        public DbSet<BlogPost> BlogPosts { get; set; }
+        public DbSet<Like> Likes { get; set; }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             base.OnModelCreating(modelBuilder);
 
-            // Add any additional configuration for your models here, if needed
+            modelBuilder.Entity<BlogPost>()
+                .HasKey(b => b.Id);
+
+            modelBuilder.Entity<BlogPost>()
+                .HasMany(b => b.Likes)
+                .WithOne(l => l.BlogPost)
+                .HasForeignKey(l => l.BlogPostId);
+
+            modelBuilder.Entity<Like>()
+                .HasKey(l => l.Id);
         }
     }
 }
