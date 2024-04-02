1548de74adc7d89d0c68597cadcc2d5e533685a1
Kaden de Frece <129616690+zoroartico@users.noreply.github.com>
2024-04-01 18:33:36 -0600

    Added Role and Admin User Initialization

---
+++ Data/AdminInitializer.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace _2211_Final_Project_TGM_Blog.Data
+{
+    public class AdminInitializer
+    {
+        public static async Task CreateUsers(IServiceProvider serviceProvider)
+        {
+            using (var scope = serviceProvider.CreateScope())
+            {
+                var userName = "admin";
+                var email = "admin@admin.admin";
+                var password = "admin";
+
+                var userManager = scope.ServiceProvider.GetRequiredService<UserManager<IdentityUser>>();
+
+                var userCheck = await userManager.FindByEmailAsync(email);
+                if (userCheck == null)
+                {
+                    var user = new IdentityUser { UserName = userName, Email = email , PasswordHash = password};
+                    await userManager.CreateAsync(user);
+                    await userManager.AddToRoleAsync(user, "Agent");
+                }
+                
+            }
+        }
+
+    }
+}
--- Program.cs
+++ Program.cs
@@ -3,6 +3,9 @@ using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 
 
+using Microsoft.Extensions.Logging;
+
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -12,6 +15,7 @@ builder.Services.AddDbContext<ApplicationDbContext>(options =>
 builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
 builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+    .AddRoles<IdentityRole>()
     .AddEntityFrameworkStores<ApplicationDbContext>();
 
 // Add services to the container.
@@ -43,5 +47,9 @@ app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
 app.MapRazorPages();
+
+await RolesInitializer.CreateRoles(app.Services);
+await AdminInitializer.CreateUsers(app.Services);
+
 
 app.Run();
