e5d39722b133e835a98781d196dcd1a252c8308a
Kaden de Frece <129616690+zoroartico@users.noreply.github.com>
2024-04-02 21:25:08 -0600

    CodeCleanup. chnaging email to be email instead of username for autocomplete as they will be different in this application. removed AdminInitializer becuase it's useless. working on UserAccounts view as WIP

--- Areas/Identity/Pages/Account/Register.cshtml.cs
+++ Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -122,7 +122,7 @@ namespace _2211_Final_Project_TGM_Blog.Areas.Identity.Pages.Account
             {
                 var user = CreateUser();
 
-                await _userStore.SetUserNameAsync(user, Input.Email, CancellationToken.None);
+                await _userStore.SetUserNameAsync(user, Input.Username, CancellationToken.None);
                 await _emailStore.SetEmailAsync(user, Input.Email, CancellationToken.None);
                 var result = await _userManager.CreateAsync(user, Input.Password);
                     
--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -31,20 +31,6 @@ namespace _2211_Final_Project_TGM_Blog.Controllers
             return RedirectToAction("Login", "Account", new { area = "Identity" });
         }
 
-        public IActionResult Register()
-        {
-            //if (User.IsInRole == "Admin")
-            return RedirectToAction("Register", "Account", new { area = "Identity" });
-        }
-
-        [Route("/admin/register")]
-        public IActionResult RegisterAdmin()
-        {
-            var roles = RoleManager.Roles.Select(r => new SelectListItem { Value = r.Name, Text = r.Name }).ToList();
-            ViewBag.Roles = roles; // Pass roles to the view bag
-            return View();
-        }
-
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
